ad3a9042efe2cf6ed67cfb7331dd4e6ff7bb2b21
Nick Snyder <n.a.snyder@comcast.net>
2017-09-03 15:59:31 -0400

    Initial setup of main page.  Working game info modal.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('#infoModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var game = button.data('game') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text(game);
+  modal.find('.modal-body img').attr("src", $($(button).children(".gameImg")).attr("src"));
+})
+});
