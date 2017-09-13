4ad0018170ac8fa80a1e339e557fcadb508362e6
Nick Snyder <n.a.snyder@comcast.net>
2017-09-13 19:35:52 -0400

    Added readme.txt files to games.  Adds info from readme to info modal

---
+++ games/ConnectFour/readme.txt
@@ -0,0 +1,6 @@
+Players: 2<br/><br/>
+
+Rules: Players choose either black or red and who goes first.  Players take turns dropping their chips from the top of the board.
+First player to get 4 of their colored chips in a row, horizontally, vertically, or diagonally, wins.
+If no player was able to get 4 in a row and the board is full, the game is a draw.
+Winner goes first next round.
--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ $(document).ready(function(){
     var button = $(event.relatedTarget); // Button that triggered the modal
     var game = button.data('game'); // Extract info from data-* attributes
     var link = $(button).attr("gameLink");
+    var readme = $(button).attr("readmeURL");
     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this);
     modal.find('.modal-title').text(game);
     modal.find('.modal-body img').attr("src", $($(button).children(".gameImg")).attr("src"));
     modal.find('.modal-footer a').attr("href", link);
-  })
+
+    var client = new XMLHttpRequest();
+    client.open('GET', readme);
+    client.onreadystatechange = function() {
+      modal.find("#modalInfo").html(client.responseText);
+    }
+    client.send();
+
+  });
 });
